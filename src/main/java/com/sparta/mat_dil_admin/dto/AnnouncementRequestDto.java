19431f58d6d9c6f028fa2a6b7c3a3882b07f311f
tjdrbs0712 <102494118+tjdrbs0712@users.noreply.github.com>
2024-06-22 23:59:29 +0900

    [Feat] 공지글 등록 기능 구현 #1

---
+++ src/main/java/com/sparta/mat_dil_admin/dto/AnnouncementRequestDto.java
@@ -0,0 +1,9 @@
+package com.sparta.mat_dil_admin.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AnnouncementRequestDto {
+    private String title;
+    private String contents;
+}
---
+++ src/main/java/com/sparta/mat_dil_admin/repository/AnnouncementPostRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.mat_dil_admin.repository;
+
+import com.sparta.mat_dil_admin.entity.AnnouncementPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnnouncementPostRepository extends JpaRepository<AnnouncementPost, Long> {
+}
